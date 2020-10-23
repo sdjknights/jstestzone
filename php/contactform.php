71637cfb125245a9c514dca5a2ea2e26e622c6d3
sdjknights <67291759+sdjknights@users.noreply.github.com>
2020-10-23 14:26:07 +0100

    Updating files. Adding emailform.html and contactform.php

---
+++ php/contactform.php
@@ -0,0 +1,21 @@
+<!-- Start php -->
+<?php
+
+
+// Check if "submit" (a post thing) is set
+if (isset($_POST["submit"])) {
+	// create variable "name" from "name" from POST
+	$name = $_POST["name"];
+	$mailFrom = $_POST["mail"];
+	$message = $_POST["message"];
+
+	
+	$mailTo = "sdjknights@hotmail.com"
+	$headers = "From: ".$mailFrom;
+		// /n = line break
+	$txt = "You have received an email from ".$name.".\n\n".$message;
+
+
+	mail($mailTo, $subject, $txt, $headers);
+	header("Location: index.php?mailsend");
+}
